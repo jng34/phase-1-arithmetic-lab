9cfa1830ad92a93f7e4e737b6a950217bd95c8e4
JamesNg <njames15@gmail.com>
2022-04-25 19:07:51 -0400

    commit tree 84bd1ae23a80eb9f8ebab68f62b54d5b80559954

--- index.js
+++ index.js
@@ -1,2 +1,18 @@
 
 // Write your code here
+
+//Define & assign values for num1 & num2, then define multipy as product of both
+const num1 = 31;
+const num2 = 2;
+const multiply = num1*num2;
+
+//Define random 
+const random = Math.ceil(Math.random() * 10) + 1;
+
+//Define & assign num3 and num 4, then define mod 
+const num3 = 12;
+const num4 = 8;
+const mod = num3 % num4;
+
+//Define max to return 20 as max in a set
+const max = Math.max(3, 12, 20, 15);
